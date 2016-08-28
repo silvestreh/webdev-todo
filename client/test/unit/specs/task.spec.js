b6b5f6ae06ceb6e488d4ad374b2d699dd28dcae5
Silvestre Herrera <silvestre.herrera@gmail.com>
2016-08-27 22:45:16 -0300

    Fix double quotes

--- client/test/unit/specs/task.spec.js
+++ client/test/unit/specs/task.spec.js
@@ -2,25 +2,39 @@ import Vue from 'vue';
 import task from 'src/components/task';
 
 describe('task.vue', () => {
-    it('should render correct contents', () => {
-        const data = {
-            task: 'Learn Javascript',
-            priority: 'high',
-            completed: true,
-        };
+    let vm;
 
-        const vm = new Vue({
-            template: '<div><task :data="data"></task></div>',
+    beforeEach((done) => {
+        vm = new Vue({
+            template: '<div><task v-ref:task-component :data="task"></task></div>',
+
+            data() {
+                return {
+                    task: {
+                        _id: 1,
+                        task: 'Learn Vue.js!',
+                        priority: 'high',
+                        completed: true,
+                    },
+                };
+            },
 
             components: {
                 task,
             },
-
-            data: () => ({
-                data,
-            }),
         }).$mount();
 
-        expect(vm.$el.querySelector('.task span').textContent).to.contain('Learn Javascript');
+        vm.$nextTick(() => done());
+    });
+
+    it('should render the task with the correct data', () => {
+        expect(vm.$el.querySelector('.task span').textContent).to.contain('Learn Vue.js!');
+    });
+
+    it('should be able to toggle the task form', (done) => {
+        const taskComponent = vm.$refs.taskComponent;
+        taskComponent.showForm();
+        expect(vm.$el.querySelector('.task span').textContent).to.contain('Learn Vue.js!');
+        done();
     });
 });
